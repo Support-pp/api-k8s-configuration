79c812ebdd5a87f5d3191fb2f0e2476ef9a0d538
Johannes Waigel <waigel@sipgate.de>
2021-11-13 02:48:38 +0100

    :rocket: add default index page when no rule match

--- scripts/k8s-deploy.sh
+++ scripts/k8s-deploy.sh
@@ -5,5 +5,6 @@ set -e
 export GITHUB_SHA=$(git rev-parse --short HEAD)
 export APP="spp-api-gateway"
 export DOMAIN="api.support-pp.de"
+export IMAGE="docker-registry.k8net.de/supportpp/$APP:latest"
 
 kubectl kustomize k8s/template | envsubst > k8s/prod.yml
